db5480d8c291e7c03b171ea774e688c1eacc8f90
Aditya Khote <21csme004aditya.gmail.com>
2024-09-18 01:32:13 +0530

    Changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // MongoDB connection
 mongoose
